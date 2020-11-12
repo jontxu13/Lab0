aef559f4e51db1703752c40d361fdd54b9c551e7
jontxu13 <jontxuds13@gmail.com>
2020-11-12 12:17:35 +0100

    lab 5

--- js/ShowQuestionsAjax.js
+++ js/ShowQuestionsAjax.js
@@ -1,16 +1,11 @@
-function showQuestionAjax(){   
-    if (window.XMLHttpRequest){
-        //code for IE7+, Firefox, Chrome, Opera, Safari
-        xmlhttp=new XMLHttpRequest();
-    }else{
-        // code for IE6, IE5
-        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    xmlhttp.onreadystatechange=function(){
-        if(xmlhttp.readyState==4 && xmlhttp.status==200){
-            document.getElementById("txtHint").innerHTML=xmlhttp.responseText;
+function showQAjax() {
+    //code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            document.getElementById("txtHint").innerHTML = xmlhttp.responseText;
         }
     }
-    xmlhttp.open("GET", "ShowXmlQuestions.php", true);
+    xmlhttp.open("GET", "ShowXmlQuestionsAjax.php", true);
     xmlhttp.send();
 }
--- php/HandlingQuizesAjax.php
+++ php/HandlingQuizesAjax.php
@@ -5,18 +5,21 @@
 	<?php include '../html/Head.html' ?>
 	<script src="../js/jquery-3.4.1.min.js"></script>
 	<script src="../js/ShowImageInForm.js"></script>
+	<script src="../js/ShowQuestionsAjax.js"></script>
 	<!--<script src="../js/ValidateFieldsQuestion.js"></script>-->
 	<style>
 		.table_QuestionForm {
 			margin: auto;
 			text-align: center;
 		}
+
 		sup {
 			color: red;
 		}
+
 		h2 {
-            color: darkblue;
-        }
+			color: darkblue;
+		}
 	</style>
 </head>
 
@@ -28,57 +31,57 @@
 			<!--<form id='fquestion' name='fquestion' action=’AddQuestion.php’> POST porque envia imagen-->
 			<!--<form id='fquestion' name='fquestion' method='POST' enctype='multipart/form-data' action='AddQuestionWithImage.php'>-->
 			<?php echo "<form id='fquestion' name='fquestion' method='POST' enctype='multipart/form-data' action='AddQuestionWithImage.php?logInMail=$logInMail'>"; ?>
-				<table class="table_QuestionForm">
-					<tr>
-						<th>
-							<h2>Insertar pregunta</h2><br />
-						</th>
-					</tr>
-					<tr>
-						<td>Direccion de correo<sup>*</sup> <input type="text" size="75" id="dirCorreo" name="Direccion de correo" value="<?php echo $logInMail; ?>" readonly></td>
-					</tr>
-					<tr>
-						<td>Enunciado de pregunta<sup>*</sup> <input type="text" size="75" id="pregunta" name="Pregunta"></td>
-					</tr>
-					<tr>
-						<td>Respuesta correcta<sup>*</sup> <input type="text" size="75" id="respuestaCorrecta" name="Respuesta correcta"></td>
-					</tr>
-					<tr>
-						<td>Respuesta incorrecta 1<sup>*</sup> <input type="text" size="75" id="respuestaIncorrecta1" name="Respuesta incorrecta 1"></td>
-					</tr>
-					<tr>
-						<td>Respuesta incorrecta 2<sup>*</sup> <input type="text" size="75" id="respuestaIncorrecta2" name="Respuesta incorrecta 2"></td>
-					</tr>
-					<tr>
-						<td>Respuesta incorrecta 3<sup>*</sup> <input type="text" size="75" id="respuestaIncorrecta3" name="Respuesta incorrecta 3"></td>
-					</tr>
-					<tr>
-						<td>Tema<sup>*</sup> <input type="text" size="50" id="tema" name="tema"></td>
-					</tr>
-					<tr>
-						<td>
-							Complejidad<sup>*</sup>
-							<select id="complejidad" name="complejidad">
-								<option value="1">Baja</option>
-								<option value="2" selected>Media</option>
-								<option value="3">Alta</option>
-							</select>
-						</td>
-					</tr>
-					<tr>
-						<td><input type="file" id="file" accept="image/*" name="file">
-							<div id="imgDynamica"></div>
-						</td>
-					</tr>
-					<tr>
-						<td><input type="button" id="enviar" value="Insertar pregunta" onclick="showQuestionsAjax()"> <input type="button" id="showq" value="Ver preguntas"> <input type="reset" id="reset" value="Limpiar"></td>
-					</tr>
-				</table>
+			<table class="table_QuestionForm">
+				<tr>
+					<th>
+						<h2>Insertar pregunta</h2><br />
+					</th>
+				</tr>
+				<tr>
+					<td>Direccion de correo<sup>*</sup> <input type="text" size="75" id="dirCorreo" name="Direccion de correo" value="<?php echo $logInMail; ?>" readonly></td>
+				</tr>
+				<tr>
+					<td>Enunciado de pregunta<sup>*</sup> <input type="text" size="75" id="pregunta" name="Pregunta"></td>
+				</tr>
+				<tr>
+					<td>Respuesta correcta<sup>*</sup> <input type="text" size="75" id="respuestaCorrecta" name="Respuesta correcta"></td>
+				</tr>
+				<tr>
+					<td>Respuesta incorrecta 1<sup>*</sup> <input type="text" size="75" id="respuestaIncorrecta1" name="Respuesta incorrecta 1"></td>
+				</tr>
+				<tr>
+					<td>Respuesta incorrecta 2<sup>*</sup> <input type="text" size="75" id="respuestaIncorrecta2" name="Respuesta incorrecta 2"></td>
+				</tr>
+				<tr>
+					<td>Respuesta incorrecta 3<sup>*</sup> <input type="text" size="75" id="respuestaIncorrecta3" name="Respuesta incorrecta 3"></td>
+				</tr>
+				<tr>
+					<td>Tema<sup>*</sup> <input type="text" size="50" id="tema" name="tema"></td>
+				</tr>
+				<tr>
+					<td>
+						Complejidad<sup>*</sup>
+						<select id="complejidad" name="complejidad">
+							<option value="1">Baja</option>
+							<option value="2" selected>Media</option>
+							<option value="3">Alta</option>
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td><input type="file" id="file" accept="image/*" name="file">
+						<div id="imgDynamica"></div>
+					</td>
+				</tr>
+				<tr>
+					<td><input type="button" id="enviar" value="Insertar pregunta"> <input type="button" id="showq" value="Ver preguntas" onclick="showQAjax()"> <input type="reset" id="reset" value="Limpiar"></td>
+				</tr>
+			</table>
 			</form>
-			<script type="text/javascript" src="../js/ShowQuestionsAjax.js"></script>
 		</div>
-        <div id="txtHint"><b>Preguntas en XML</b></div>
+		<div id="txtHint"></div>
 	</section>
+	
 	<?php include '../html/Footer.html' ?>
 </body>
 
