b74d75e032612adb86eb4be1bb15ea3c680660f7
jontxu13 <jontxuds13@gmail.com>
2020-11-19 11:33:29 +0100

    lab 6

---
+++ js/ClientVerify.js
@@ -0,0 +1,18 @@
+function verifyVIP() {
+    email = $('#dirCorreo').val();
+    if (isNaN(email)){
+        $.ajax({
+            type: "POST",
+            url: 'ClientVerifyEnrollment.php',
+            data: {"Email": email},
+            cache: false,
+            success: function(respuesta) {
+                if(respuesta=="SI"){
+                $("#vip").html("<td style=color:green aling='center'>" + respuesta + " eres VIP</td>");
+                }else{
+                $("#vip").html("<td style=color:red aling='center'>" + respuesta + " eres VIP</td>");
+                }
+            }
+        });
+    }
+}
