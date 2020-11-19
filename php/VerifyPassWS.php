280009c3e40c2565619fc54d5604a8fe8cedeb69
jontxu13 <jontxuds13@gmail.com>
2020-11-19 13:39:08 +0100

    lab 6

--- php/VerifyPassWS.php
+++ php/VerifyPassWS.php
@@ -1,25 +1,21 @@
 <?php
-
 //incluimos la clase nusoap.php
 require_once('../lib/nusoap.php');
 require_once('../lib/class.wsdlcache.php');
-
 //creamos el objeto de tipo soap_server
-$ns = "http://localhost/nusoap-0.9.5/samples";
+$ns = "../php/VerifyPassWS?wsdl";
 $server = new soap_server;
-$server->configureWSDL('VerifyPass', $ns);
+$server->configureWSDL('verifyPass', $ns);
 $server->wsdl->schemaTargetNamespace = $ns;
-
 //registramos la función que vamos a implementar
 $server->register(
-    'VerifyPass',
+    'verifyPass',
     array('x' => 'xsd:string', 'y' => 'xsd:int'),
     array('z' => 'xsd:string'),
     $ns
 );
-
 //implementamos la función
-function VerifyPass($x, $y)
+function verifyPass($x, $y)
 {
     if ($y == 1010) {
         $dictionary = fopen("../txt/toppasswords.txt", "r");
@@ -35,6 +31,7 @@ function VerifyPass($x, $y)
         return "SIN SERVICIO";
     }
 }
-
+//llamamos al método service de la clase nusoap antes obtenemos los valores de los parámetros
 if (!isset($HTTP_RAW_POST_DATA)) $HTTP_RAW_POST_DATA = file_get_contents('php://input');
 $server->service($HTTP_RAW_POST_DATA);
+?>
