54a449b09315b894ee3d6318cfdbfac68cd5ce7e
jontxu13 <jontxuds13@gmail.com>
2020-11-20 23:41:39 +0100

    lab 6

--- php/ClientVerifyPass.php
+++ php/ClientVerifyPass.php
@@ -5,7 +5,7 @@ require_once('../lib/class.wsdlcache.php');
 //creamos el objeto de tipo soapclient.
 //http://www.mydomain.com/server.php se refiere a la url
 //donde se encuentra el servicio SOAP que vamos a utilizar.
-$soapclient = new nusoap_client('../php/VerifyPassWS?wsdl', true);
+$soapclient = new nusoap_client('../php/VerifyPassWS?wsdl');
 //Llamamos la función que habíamos implementado en el Web Service
 //e imprimimos lo que nos devuelve
 if (isset($_POST['Pass'])) {
