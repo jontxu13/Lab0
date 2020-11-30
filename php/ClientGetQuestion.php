83db975416b4fa2f3c94c98fc0bd105ad1e22826
jontxu13 <jontxuds13@gmail.com>
2020-11-30 11:06:29 +0100

    lab 7

--- php/ClientGetQuestion.php
+++ php/ClientGetQuestion.php
@@ -1,3 +1,9 @@
+<?php 
+session_start();
+if(!isset($_SESSION["usuario"])) {
+    header("location:LogIn.php");
+} else {
+?>
 <?php
 require '../lib/nusoap.php';
 
@@ -42,7 +48,7 @@ $soapclient = new nusoap_client('http://localhost/sw20/php/GetQuestionWS.php?wsd
     <?php include '../php/DbConfig.php' ?>
     <section class="main" id="s1">
         <div>
-        <?php echo "<form id='fidpregunta' name='fidpregunta' method='POST' enctype='multipart/form-data' action='ClientGetQuestion.php?logInMail=$logInMail'>"; ?>
+        <?php echo "<form id='fidpregunta' name='fidpregunta' method='POST' enctype='multipart/form-data' action='ClientGetQuestion.php'>"; ?>
                 <table class="table_fregister">
                     <td>Id de la pregunta a buscar: <input type="number" id="idQuestion" name="idQuestion"></td>
                     </tr>
@@ -65,3 +71,6 @@ $soapclient = new nusoap_client('http://localhost/sw20/php/GetQuestionWS.php?wsd
     <?php include '../html/Footer.html' ?>
 </body>
 </html>
+<?php
+}
+?>
